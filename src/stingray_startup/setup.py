3cd74de3eb6051c35b9b46222aa88e9462733226
Vladislav Plotnikov <vladik1209@gmail.com>
2023-07-20 17:55:50 +0300

    builds

--- src/stingray_startup/setup.py
+++ src/stingray_startup/setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup
-import os
-from glob import glob
 
 package_name = 'stingray_startup'
 
 setup(
     name=package_name,
-    version='0.2.0',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +20,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'qr_trigger = stingray_startup.qr_trigger:main',
         ],
     },
 )
