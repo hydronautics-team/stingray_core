f57902df80decdbc672a9d5f9c9a9fdd093e933b
Nikita Bulatov <n.bulatov@1440.space>
2025-11-15 22:48:32 +0300

    feat;Докер для малины(ожидает тестов на малине)

---
+++ docker_rpi/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME=stingray_core_rpi
+
+echo "[INFO] Сборка Docker-образа: $IMAGE_NAME"
+docker build -t $IMAGE_NAME -f docker_rpi/Dockerfile .
+
+echo "[INFO] Образ успешно собран."
---
+++ docker_rpi/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/humble/setup.bash
+
+# Если проект уже собран
+if [ -f  /stingray_core/install/setup.bash ]; then
+  echo "[INFO] Используем существующую сборку."
+  source /stingray_core/install/setup.bash
+else
+  echo "[INFO] Локальная сборка не найдена (install/setup.bash отсутствует)"
+fi
+
+exec "$@"
