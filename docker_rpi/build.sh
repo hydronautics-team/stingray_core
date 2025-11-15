5b318285ca2ad8064cc2874f9c03ffdd5a93295a
Nikita Bulatov <n.bulatov@1440.space>
2025-11-15 22:54:51 +0300

    fix: добавлен аргумент для сборки под arm32v7 в скрипте билда для rpi

--- docker_rpi/build.sh
+++ docker_rpi/build.sh
@@ -4,6 +4,6 @@ set -e
 IMAGE_NAME=stingray_core_rpi
 
 echo "[INFO] Сборка Docker-образа: $IMAGE_NAME"
-docker build -t $IMAGE_NAME -f docker_rpi/Dockerfile .
+docker build -t --platform linux/arm32v7 $IMAGE_NAME -f docker_rpi/Dockerfile .
 
 echo "[INFO] Образ успешно собран."
