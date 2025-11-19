75d2b1ecba4c95f149ea4f159f33831a87037427
Nikita Bulatov <n.bulatov@1440.space>
2025-11-19 22:29:39 +0300

    frat;Завели гидропротокл на росе

--- src/stingray_core_communication/src/thrusters_driver_node.cpp
+++ src/stingray_core_communication/src/thrusters_driver_node.cpp
@@ -6,58 +6,118 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/u_int8_multi_array.hpp"
 
+#include "hydrolib_log_distributor.hpp"
+#include "hydrolib_bus_datalink_stream.hpp"
+#include "hydrolib_bus_application_master.hpp"
+
 namespace stingray_core
 {
 
-class ThrustersDriverNode : public rclcpp::Node
-{
-public:
-    ThrustersDriverNode() : Node("thrusters_driver_node")
+    class TestLogStream
     {
-        thrusters_sub_ = this->create_subscription<std_msgs::msg::UInt8MultiArray>(
-            "/thrusters/cmd", 10, std::bind(&ThrustersDriverNode::thrustersCallback, this, std::placeholders::_1));
+    };
 
-        serial_pub_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>("serial_write", 10);
-        RCLCPP_INFO(this->get_logger(), "Thrusters driver node initialized");
-    }
-
-private:
-    void thrustersCallback(const std_msgs::msg::UInt8MultiArray::SharedPtr msg)
+    int write([[maybe_unused]] TestLogStream &stream, const void *dest, unsigned length)
     {
-        if (!msg->data.empty())
+        for (unsigned i = 0; i < length; i++)
         {
-            std::vector<uint8_t> packet = createPacket(msg->data);
+            std::cout << (reinterpret_cast<const char *>(dest))[i];
+        }
+        return length;
+    }
+    class ThrustersDriverNode;
+    int write(ThrustersDriverNode &stream, const void *data, unsigned lenght);
+    int read(ThrustersDriverNode &stream, void *data, unsigned lenght);
+
+    constexpr char *_ser = "Serializer";
+    constexpr char *_distr = "[%s] [%l] %m\n";
+
+    TestLogStream log_stream;
+    hydrolib::logger::LogDistributor<TestLogStream> distributor{_distr, log_stream};
+    hydrolib::logger::Logger<hydrolib::logger::LogDistributor<TestLogStream>> logger{_ser, 0, distributor};
+
+    class ThrustersDriverNode : public rclcpp::Node
+    {
+    public:
+        ThrustersDriverNode() : Node("thrusters_driver_node"),
+                                _stream_manager(2, *this, logger),
+                                _stream(_stream_manager, 1),
+                                _master(_stream, logger)
+        {
+            thrusters_sub_ = this->create_subscription<std_msgs::msg::UInt8MultiArray>(
+                "/thrusters/cmd", 10, std::bind(&ThrustersDriverNode::thrustersCallback, this, std::placeholders::_1));
+
+            serial_pub_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>("serial_write", 10);
+
+            RCLCPP_INFO(this->get_logger(), "Thrusters driver node initialized");
+        }
+
+        int write(const void *data, unsigned lenght)
+        {
+
+            std::vector<uint8_t> packet = createPacket(data, lenght);
             auto serial_msg = std_msgs::msg::UInt8MultiArray();
             serial_msg.data = packet;
             serial_pub_->publish(serial_msg);
-
-            RCLCPP_INFO(this->get_logger(), "Sent %zu bytes to serial", packet.size());
         }
-    }
 
-    rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr thrusters_sub_;
-
-    rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr serial_pub_;
-
-    std::vector<uint8_t> createPacket(const std::vector<uint8_t> &thrusters)
-    {
-        std::vector<uint8_t> packet;
-        // Заголовок пакета
-        packet.push_back(0xFF);
-        packet.push_back(0xFD);
-
-        // Закидываем значения в массив и считаем контрольную сумму
-        uint8_t checksum = 0;
-        for (auto value : thrusters)
+        int read(void *data, unsigned lenght)
         {
-            packet.push_back(value);
-            checksum ^= value;
+            return 0; /* TODO сделать норм метод*/
         }
-        packet.push_back(checksum);
 
-        return packet;
+    private:
+        void thrustersCallback(const std_msgs::msg::UInt8MultiArray::SharedPtr msg)
+        {
+            if (!msg->data.empty())
+            {
+                const void *data = msg->data.data();
+                unsigned length = static_cast<unsigned>(msg->data.size());
+                _master.Write(data, 0, length);
+                RCLCPP_INFO(this->get_logger(), "Sent %zu bytes to serial", length);
+            }
+        }
+
+        rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr thrusters_sub_;
+
+        rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr serial_pub_;
+
+        std::vector<uint8_t> createPacket(const void *data, unsigned lenght)
+        {
+            std::vector<uint8_t> packet;
+            auto value = static_cast<const uint8_t *>(data);
+            for (int i = 0; i < lenght; i++)
+            {
+                packet.push_back(value[i]);
+            }
+
+            return packet;
+        }
+
+        hydrolib::bus::datalink::StreamManager<ThrustersDriverNode,
+                                               hydrolib::logger::LogDistributor<TestLogStream>>
+            _stream_manager;
+
+        hydrolib::bus::datalink::Stream<ThrustersDriverNode,
+                                        hydrolib::logger::LogDistributor<TestLogStream>>
+            _stream;
+
+        hydrolib::bus::application::Master<hydrolib::bus::datalink::Stream<ThrustersDriverNode,
+                                                                           hydrolib::logger::LogDistributor<TestLogStream>>,
+                                           hydrolib::logger::LogDistributor<TestLogStream>>
+            _master;
+
+    }; // class ThrustersDriverNode
+
+    int write(ThrustersDriverNode &stream, const void *data, unsigned lenght)
+    {
+        return stream.write(data, lenght);
     }
-}; // class ThrustersDriverNode
+
+    int read(ThrustersDriverNode &stream, void *data, unsigned lenght)
+    {
+        return stream.read(data, lenght);
+    }
 
 } // namespace stingray_core
 
