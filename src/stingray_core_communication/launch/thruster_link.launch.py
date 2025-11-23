abcfd233c09f5deae993bb8f433b9c39efdc2896
Nikita Bulatov <n.bulatov@1440.space>
2025-11-23 21:16:24 +0300

    Работает, дополнительно нужно потестить

---
+++ src/stingray_core_communication/launch/thruster_link.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch.actions import GroupAction, IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node, PushRosNamespace
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+import os
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='stingray_core_communication',                 # твой пакет
+            executable='thrusters_driver_node',      # имя исполняемого файла после сборки
+            name='thrusters_driver_node',
+            output='screen'
+        )
+    ])
