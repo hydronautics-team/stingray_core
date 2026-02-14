d95ca722bdceca91148fe7bfcd4a157c2684e89c
WaspishRaccoon <p.v.elesin@gmail.com>
2026-02-14 16:02:57 +0300

    f/690 - add pressure_sensor node

---
+++ src/pressure_sensor/include/pressure_sensor/pressure_sensor.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64.hpp>
+#include <std_msgs/msg/string.hpp>
+
+#define DEFAULT_DUMP_PARAM 1.0
+
+namespace stingray_core::pressure_sensor {
+
+struct PressureSensorConfig{
+    PressureSensorConfig(const rclcpp::Node::SharedPtr& node)
+    : dump_param(node->declare_parameter<double>("dump_param", DEFAULT_DUMP_PARAM))
+    {}
+    const double  dump_param;
+};
+
+class PressureSensor {
+   public:
+    explicit PressureSensor(
+        rclcpp::NodeOptions options = rclcpp::NodeOptions());
+
+    void spin() { 
+        rclcpp::spin(node_);
+    }
+
+    rclcpp::Logger get_logger() const { return node_->get_logger(); }
+
+   private:
+    void data_raw_callback(const std_msgs::msg::String::ConstSharedPtr& msg);
+    void publish_depth(const double depth);
+
+    rclcpp::Node::SharedPtr node_;
+    PressureSensorConfig config_;
+
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr depth_pub_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr data_raw_sub_;
+};
+
+}  // namespace stingray_core::pressure_sensor
