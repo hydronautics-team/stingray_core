760dc36a5b9ec562aec60ad974326e52b189d346
WaspishRaccooon <p.v.elesin@gmail.com>
2026-02-19 20:48:48 +0300

    add params file

--- src/power_control/launch/power_gpio.launch.py
+++ src/power_control/launch/power_gpio.launch.py
@@ -1,30 +1,30 @@
+import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    gpio_chip_arg = DeclareLaunchArgument('gpio_chip', default_value='gpiochip0')
-    gpio_line_arg = DeclareLaunchArgument('gpio_line', default_value='-1')
-    active_high_arg = DeclareLaunchArgument('active_high', default_value='true')
-    default_on_arg = DeclareLaunchArgument('default_on', default_value='true')
-
+    default_params_file = PathJoinSubstitution([
+        FindPackageShare('power_control'),
+        'config',
+        'power_gpio.param.yaml'
+    ])
+    
     return LaunchDescription([
-        gpio_chip_arg,
-        gpio_line_arg,
-        active_high_arg,
-        default_on_arg,
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=default_params_file,
+            description='Path to YAML parameters file'
+        ),
+
         Node(
             package='power_control',
             executable='power_gpio_node',
             name='power_gpio',
             output='screen',
-            parameters=[{
-                'gpio_chip': LaunchConfiguration('gpio_chip'),
-                'gpio_line': LaunchConfiguration('gpio_line'),
-                'active_high': LaunchConfiguration('active_high'),
-                'default_on': LaunchConfiguration('default_on'),
-            }],
+            parameters=[LaunchConfiguration('params_file')],
+            emulate_tty=True,
         ),
     ])
