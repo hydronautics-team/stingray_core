dbde39ffc74adebeb79b5684f28ab6cc5021bcf2
Nikita Bulatov <n.bulatov@1440.space>
2026-04-16 21:43:17 +0300

    Готово к тестам на железке

---
+++ src/stingray_core_communication/stingray_core_communication/src/panel_link_node.cpp
@@ -0,0 +1,70 @@
+#include <array>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <vector>
+
+#include "link_node_base.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/u_int8_multi_array.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+namespace stingray_core
+{
+
+using namespace std::chrono_literals;
+class PanelLinkNode : public baseLink::LinkNodeBase
+{
+public:
+    PanelLinkNode() : LinkNodeBase("Panel_driver_node", 2, 3)
+
+    {
+        thrusters_sub_ = this->create_subscription<std_msgs::msg::UInt8MultiArray>(
+            "/thruster/cmd", 10, std::bind(&PanelLinkNode::thrustersCallback, this, std::placeholders::_1));
+        data_disp_pub_ = this->create_publisher<std_msgs::msg::String>("data_disp", 10);
+        auto timer_callback = [this]() -> void
+        {
+            serialWrite(static_cast<const void *>(displayMemomry_.data()), VmaStatusAddr_, VmaStatusLen_);
+            serialRead(static_cast<void *>(displayMemomry_.data()), MissionAddr_, 1);
+            publishRaw(displayMemomry_[MissionAddr_]);
+        };
+        timer_ = this->create_wall_timer(500ms, timer_callback);
+        RCLCPP_INFO(this->get_logger(), "Thrusters driver node initialized");
+    }
+
+private:
+    void publishRaw(const uint8_t value)
+    {
+        auto msg = std_msgs::msg::String();
+        msg.data = std::to_string(value);
+        data_disp_pub_->publish(std::move(msg));
+    }
+
+    void thrustersCallback(const std_msgs::msg::UInt8MultiArray::SharedPtr msg)
+    {
+        if (!msg->data.empty())
+        {
+            std::memcpy(&displayMemomry_, msg->data.data(), msg->data.size());
+        }
+    }
+    rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr thrusters_sub_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr data_disp_pub_;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    static constexpr uint8_t VmaStatusLen_{10};
+    static constexpr uint8_t VmaStatusAddr_{0};
+    static constexpr uint8_t MissionAddr_{11};
+    std::array<uint8_t, 146> displayMemomry_{};
+}; // class PanelLinkNode
+
+} // namespace stingray_core
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<stingray_core::PanelLinkNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
--- src/stingray_core_launch/launch/run_rov.launch.py
+++ src/stingray_core_launch/launch/run_rov.launch.py
@@ -9,16 +9,18 @@ def generate_launch_description():
     comm_pkg = get_package_share_directory('stingray_core_communication')
     control_pkg = get_package_share_directory('stingray_core_control')
     pressure_pkg = get_package_share_directory('pressure_sensor')
-    ms5837_pkg = get_package_share_directory('ms5837_pressure_sensor')
+    # ms5837_pkg = get_package_share_directory('ms5837_pressure_sensor')
     vectornav_pkg = get_package_share_directory('vectornav')
 
     pressure_link_launch = os.path.join(comm_pkg, 'launch', 'pressure_link.launch.py')
     thruster_link_launch = os.path.join(comm_pkg, 'launch', 'thruster_link.launch.py')
+    panel_link_launch = os.path.join(comm_pkg, 'launch', 'panel_link.launch.py')
     pressure_params = os.path.join(comm_pkg, 'params', 'pressure.params.yaml')
+    panel_params = os.path.join(comm_pkg, 'params', 'panel.params.yaml')
     thruster_params = os.path.join(comm_pkg, 'params', 'thruster.params.yaml')
     core_control_launch = os.path.join(control_pkg, 'launch', 'stingray_core_control.launch.py')
     pressure_sensor_launch = os.path.join(pressure_pkg, 'launch', 'pressure_sensor.launch.py')
-    ms5837_launch = os.path.join(ms5837_pkg, 'launch', 'ms5837.launch.py')
+    # ms5837_launch = os.path.join(ms5837_pkg, 'launch', 'ms5837.launch.py')
     vectornav_launch = os.path.join(vectornav_pkg, 'launch', 'vectornav.launch.py')
 
     thruster_link = IncludeLaunchDescription(
@@ -36,10 +38,18 @@ def generate_launch_description():
             'params_file': pressure_params,
         }.items()
     )
-
-    pressure_sensor = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(pressure_sensor_launch)
+    
+    panel_link = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(panel_link_launch),
+        launch_arguments={
+            'ns': 'panel',
+            'params_file': panel_params,
+        }.items()
     )
+
+    # pressure_sensor = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(pressure_sensor_launch)
+    # )
 
     core_control = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(core_control_launch)
@@ -56,7 +66,8 @@ def generate_launch_description():
     return LaunchDescription([
         thruster_link,
         core_control,
-        pressure_link,
-        pressure_sensor,
+        # pressure_link,
+        panel_link,
+        # pressure_sensor,
         vectornav,
     ])
