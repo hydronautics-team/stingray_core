25f6757f9f53ae67343fbff3b1399647c9876b2e
WaspishRaccoon <p.v.elesin@gmail.com>
2026-04-25 19:07:05 +0300

    add test lights blinking

--- src/stingray_core_devices/lights_device/include/lights_device/lights_device.hpp
+++ src/stingray_core_devices/lights_device/include/lights_device/lights_device.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "chrono"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/u_int8_multi_array.hpp"
@@ -20,9 +21,19 @@ private:
     void data_raw_callback(const std_msgs::msg::Int32::ConstSharedPtr &msg);
     void publish_cmd(uint8_t command);
 
+    void start_blinking(int period_ms, uint8_t brightness);
+    void stop_blinking();
+    void blink_timer_callback();
+
     rclcpp::Node::SharedPtr node_;
     rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr lights_pub_;
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr mode_sub_;
+
+    rclcpp::TimerBase::SharedPtr blink_timer_n_times_;
+    bool is_blinking_ = false;
+    bool blink_state_ = false;
+    uint8_t blink_brightness_ = 255;
+    int blink_period_ms_ = 500;
 };
 
 } // namespace stingray_core::lights_device
--- src/stingray_core_devices/lights_device/src/lights_device.cpp
+++ src/stingray_core_devices/lights_device/src/lights_device.cpp
@@ -7,29 +7,73 @@ namespace stingray_core::lights_device
 LightsControl::LightsControl(rclcpp::NodeOptions options)
     : node_(rclcpp::Node::make_shared("lights_device", std::move(options))),
       lights_pub_(node_->create_publisher<std_msgs::msg::UInt8MultiArray>("/lights/brightness", 10)),
-      mode_sub_(node_->create_subscription<std_msgs::msg::Int32>("/lights/mode", 10,
-                                                                  [this](const std_msgs::msg::Int32::ConstSharedPtr &msg)
-                                                                  { this->data_raw_callback(msg); }))
+      mode_sub_(node_->create_subscription<std_msgs::msg::Int32>(
+          "/lights/mode", 10, [this](const std_msgs::msg::Int32::ConstSharedPtr &msg) { this->data_raw_callback(msg); }))
 {
     RCLCPP_INFO(node_->get_logger(), "Lights device node initialized");
 }
 
 void LightsControl::data_raw_callback(const std_msgs::msg::Int32::ConstSharedPtr &msg)
 {
-    if (msg->data == 1)
+    // Расширьте команды
+    switch (msg->data)
     {
-        RCLCPP_DEBUG(node_->get_logger(), "lights: 1");
-        publish_cmd(255);
+    case 0: // OFF
+        lights_off();
+        break;
+    case 1: // ON
+        lights_on();
+        break;
+    case 2: // BLINK SLOW (1 Hz)
+        start_blinking(1000, 255);
+        break;
+    case 3: // BLINK FAST (5 Hz)
+        start_blinking(200, 255);
+        break;
+    default:
+        RCLCPP_WARN(node_->get_logger(), "Unknown command! %d", msg->data);
     }
-    else if (msg->data == 0)
-    {
-        RCLCPP_DEBUG(node_->get_logger(), "lights: 255");
+}
 
-        publish_cmd(0);
+void LightsControl::start_blinking(int period_ms, uint8_t brightness)
+{
+    stop_blinking();
+    is_blinking_ = true;
+    blink_brightness_ = brightness;
+    blink_period_ms_ = period_ms;
+    blink_state_ = true;
+
+    blink_timer_ = node_->create_wall_timer(std::chrono::milliseconds(period_ms / 2), // пол периода для каждого состояния
+                                            [this]() { this->blink_timer_callback(); });
+
+    publish_cmd(blink_brightness_);
+    RCLCPP_INFO(node_->get_logger(), "Started blinking with period %d ms, brightness %d", period_ms, brightness);
+}
+
+void LightsControl::stop_blinking()
+{
+    if (blink_timer_)
+    {
+        blink_timer_->cancel();
+        blink_timer_.reset();
+    }
+    is_blinking_ = false;
+    blink_state_ = false;
+}
+
+void LightsControl::blink_timer_callback()
+{
+    if (!is_blinking_)
+        return;
+
+    blink_state_ = !blink_state_;
+    if (blink_state_)
+    {
+        publish_cmd(blink_brightness_);
     }
     else
     {
-        RCLCPP_WARN(node_->get_logger(), "Unknown command! %u", msg->data);
+        publish_cmd(0);
     }
 }
 
@@ -39,6 +83,18 @@ void LightsControl::publish_cmd(uint8_t command)
     command_msg.data = {command, command};
     RCLCPP_DEBUG(node_->get_logger(), "Published command lights: %u", command);
     lights_pub_->publish(command_msg);
+}
+
+void LightsControl::lights_off()
+{
+    stop_blinking();
+    publish_cmd(0);
+}
+
+void LightsControl::lights_on()
+{
+    stop_blinking();
+    publish_cmd(255);
 }
 
 } // namespace stingray_core::lights_device
