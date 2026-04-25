66ae40916a78389c582751ba04750cf853fdcaeb
DaShakuev <dan.shakuev@yandex.ru>
2026-04-25 18:30:29 +0300

    update and fix  lights node, control from /lights/mode

--- src/stingray_core_devices/lights_device/src/lights_device.cpp
+++ src/stingray_core_devices/lights_device/src/lights_device.cpp
@@ -1,33 +1,35 @@
 #include "lights_device/lights_device.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/u_int8_multi_array.hpp"
 
 namespace stingray_core::lights_device
 {
 
 LightsControl::LightsControl(rclcpp::NodeOptions options)
     : node_(rclcpp::Node::make_shared("lights_device", std::move(options))),
-      lights_pub_(node_->create_publisher<std_msgs::msg::UInt8MultiArray>("/lights/brightness", rclcpp::SensorDataQoS())),
-      mode_sub_(node_->create_subscription<std_msgs::msg::String>("/lights/set_mode", rclcpp::SensorDataQoS(),
-                                                                  [this](const std_msgs::msg::String::ConstSharedPtr &msg)
+      lights_pub_(node_->create_publisher<std_msgs::msg::UInt8MultiArray>("/lights/brightness", 10)),
+      mode_sub_(node_->create_subscription<std_msgs::msg::Int32>("/lights/mode", 10,
+                                                                  [this](const std_msgs::msg::Int32::ConstSharedPtr &msg)
                                                                   { this->data_raw_callback(msg); }))
 {
     RCLCPP_INFO(node_->get_logger(), "Lights device node initialized");
 }
 
-void LightsControl::data_raw_callback(const std_msgs::msg::String::ConstSharedPtr &msg)
+void LightsControl::data_raw_callback(const std_msgs::msg::Int32::ConstSharedPtr &msg)
 {
-    if (msg->data == "ON")
+    if (msg->data == 1)
     {
+        RCLCPP_DEBUG(node_->get_logger(), "lights: 1");
         publish_cmd(255);
     }
-    else if (msg->data == "OFF")
+    else if (msg->data == 0)
     {
+        RCLCPP_DEBUG(node_->get_logger(), "lights: 255");
+
         publish_cmd(0);
     }
     else
     {
-        RCLCPP_WARN(node_->get_logger(), "Unknown command! %s", msg->data.c_str());
+        RCLCPP_WARN(node_->get_logger(), "Unknown command! %u", msg->data);
     }
 }
 
