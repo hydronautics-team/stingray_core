d6ba79b038a9470f93b0b3c8d0da1bdcf4a91b37
DaShakuev <dan.shakuev@yandex.ru>
2026-04-25 18:30:29 +0300

    update and fix  lights node, control from /lights/mode

--- src/stingray_core_devices/lights_device/include/lights_device/lights_device.hpp
+++ src/stingray_core_devices/lights_device/include/lights_device/lights_device.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/u_int8_multi_array.hpp"
 
 namespace stingray_core::lights_device
@@ -17,12 +17,12 @@ public:
     rclcpp::Logger get_logger() const { return node_->get_logger(); }
 
 private:
-    void data_raw_callback(const std_msgs::msg::String::ConstSharedPtr &msg);
+    void data_raw_callback(const std_msgs::msg::Int32::ConstSharedPtr &msg);
     void publish_cmd(uint8_t command);
 
     rclcpp::Node::SharedPtr node_;
     rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr lights_pub_;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr mode_sub_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr mode_sub_;
 };
 
 } // namespace stingray_core::lights_device
