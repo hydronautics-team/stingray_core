57e722673017d7f08b43d8cdd00f28352b4850ed
WaspishRaccoon <p.v.elesin@gmail.com>
2026-09-17 18:40:46 +0300

    fix

--- src/stingray_core_communication/stingray_core_communication/src/link_node_base.hpp
+++ src/stingray_core_communication/stingray_core_communication/src/link_node_base.hpp
@@ -81,6 +81,12 @@ public:
     {
         auto serial_msg = std_msgs::msg::UInt8MultiArray();
         serial_msg.data = createPacket(data, length);
+
+        RCLCPP_INFO(
+        this->get_logger(),
+        "SERIAL WRITE: length=%u",
+        length);
+        
         serial_pub_->publish(serial_msg);
         return static_cast<int>(length);
     }
